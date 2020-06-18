9e86e4957a24a5d0b3a1106abb285e7e5c03f71f
thassiov <tvmcarvalho@gmail.com>
2020-06-18 10:04:41 -0300

    chore: remove unused helper function for renaming obj properties

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,3 @@
-interface ICaseTranslator {
-    (input: string): string,
-};
-
 export function snakeCaseToCamelCase(input: string): string {
     return input.toLowerCase()
     .replace(/_([a-z])/g, i => i.toUpperCase())
@@ -11,5 +7,3 @@ export function snakeCaseToCamelCase(input: string): string {
 export function camelCaseToSnakeCase(input: string): string {
     return input.replace(/([A-Z])/g, "_$1").toLowerCase();
 }
-
-export function renameObjectProperties(obj: any, translator: ICaseTranslator) {}
