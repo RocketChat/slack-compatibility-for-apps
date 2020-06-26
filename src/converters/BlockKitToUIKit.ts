dfa5fa96ded23725e67e96380cd395982174ac10
thassiov <tvmcarvalho@gmail.com>
2020-06-26 15:27:16 -0300

    feat: add divider block's converter

--- src/converters/BlockKitToUIKit.ts
+++ src/converters/BlockKitToUIKit.ts
@@ -1,17 +1,18 @@
-import { Block, ActionsBlock, SectionBlock } from '../../vendor/slack-types';
+import { Block, ActionsBlock, SectionBlock, DividerBlock } from '../../vendor/slack-types';
 import { IBlock } from '@rocket.chat/apps-engine/definition/uikit';
 import { convertToUIKit as convertActionBlockToUIKit } from './blocks/action';
 import { convertToUIKit as convertSectionBlockToUIKit } from './blocks/section';
+import { convertToUIKit as convertDiviverBlockToUIKit } from './blocks/divider';
 
 export function convertToUIKit(blocks: Array<Block>): Array<IBlock> {
     return blocks.map((block) => {
         switch (block.type) {
             case 'action':
                 return convertActionBlockToUIKit(block as ActionsBlock);
-
             case 'section':
                 return convertSectionBlockToUIKit(block as SectionBlock);
-
+            case 'divider':
+                return convertDiviverBlockToUIKit(block as DividerBlock);
             default:
                 // @NOTE this will be dropped when filtering for truthy values
                 return null;
