3d49d978f4e27af1724ca1ad2d36389c5a8f5610
thassiov <tvmcarvalho@gmail.com>
2020-06-26 16:53:19 -0300

    chore: register 'image' block as convertable

--- src/converters/BlockKitToUIKit.ts
+++ src/converters/BlockKitToUIKit.ts
@@ -1,8 +1,9 @@
-import { Block, ActionsBlock, SectionBlock, DividerBlock } from '../../vendor/slack-types';
+import { Block, ActionsBlock, SectionBlock, DividerBlock, ImageBlock } from '../../vendor/slack-types';
 import { IBlock } from '@rocket.chat/apps-engine/definition/uikit';
 import { convertToUIKit as convertActionBlockToUIKit } from './blocks/action';
 import { convertToUIKit as convertSectionBlockToUIKit } from './blocks/section';
 import { convertToUIKit as convertDiviverBlockToUIKit } from './blocks/divider';
+import { convertToUIKit as convertImageBlockToUIKit } from './blocks/image';
 
 export function convertToUIKit(blocks: Array<Block>): Array<IBlock> {
     return blocks.map((block) => {
@@ -13,6 +14,8 @@ export function convertToUIKit(blocks: Array<Block>): Array<IBlock> {
                 return convertSectionBlockToUIKit(block as SectionBlock);
             case 'divider':
                 return convertDiviverBlockToUIKit(block as DividerBlock);
+            case 'image':
+                return convertImageBlockToUIKit(block as ImageBlock);
             default:
                 // @NOTE this will be dropped when filtering for truthy values
                 return null;
