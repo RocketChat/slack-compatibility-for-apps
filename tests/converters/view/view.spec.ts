06c511c8d501edbdecc956065ee8f150786c38ca
lolimay <lolimay@lolimay.cn>
2020-07-01 16:10:28 +0800

    test: add view converter testing template

---
+++ tests/converters/view/view.spec.ts
@@ -0,0 +1,14 @@
+describe('View converter', () => {
+
+    describe('From BlockKit to UIKit', () => {
+        it('should convert a view object from Slack to Rocket.Chat format', () => {
+
+        });
+    });
+
+    describe('From UIKit to BlockKit', () => {
+        it('should convert a view object from Rocket.Chat to Slack format', () => {
+
+        });
+    });
+});
