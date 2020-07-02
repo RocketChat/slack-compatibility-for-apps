829124a496031f3b379607040296d6b05d574d79
lolimay <lolimay@lolimay.cn>
2020-07-03 00:05:03 +0800

    Update mockView

--- tests/mocks/view/mockView.ts
+++ tests/mocks/view/mockView.ts
@@ -1,55 +1,255 @@
-import { IUIKitView, UIKitViewType, TextObjectType, BlockElementType, BlockType, IBlock, IInputBlock } from '@rocket.chat/apps-engine/definition/uikit';
+import { BlockElementType, BlockType, IInputBlock, IUIKitView, TextObjectType, UIKitViewType } from '@rocket.chat/apps-engine/definition/uikit';
 
-import { View } from '../../../vendor/slack-types';
+import { BlockKitInputBlockElementType, IBlockKitView } from '../../../src/customTypes/slack';
+
+const mockUIKitViewState: object = {
+    'block_plain-text-input': {
+        'action_plain-text-input': 'Content for plain text input'
+    },
+    'block_static-select': {
+        'action_static-select': 'value-1'
+    },
+    'block_multi-static-select': {
+        'action_multi-static-select': [
+            'value-0',
+            'value-1'
+        ]
+    }
+}
+
+const mockBlockKitViewState: IBlockKitView['state'] = {
+    values: {
+        'block_plain-text-input': {
+            'action_plain-text-input': {
+                type: BlockKitInputBlockElementType.PLAIN_TEXT_INPUT,
+                value: 'Content for plain text input'
+            }
+        },
+        'block_static-select': {
+            'action_static-select': {
+                type: BlockKitInputBlockElementType.STATIC_SELECT,
+                value: 'value-1'
+            }
+        },
+        'block_multi-static-select': {
+            'action_multi-static-select': {
+                type: BlockKitInputBlockElementType.MULTI_STATIC_SELECT,
+                value: [
+                    'value-0',
+                    'value-1'
+                ]
+            }
+        }
+    }
+}
 
 /**
  * See [here](https://api.slack.com/reference/surfaces/views) for the details.
  */
-export const mockBlockKitView: View = {
+export const mockBlockKitView: IBlockKitView = {
     // mandatory
     type: 'modal',
     title: {
         type: 'plain_text',
-        text: 'Modal Title'
+        text: 'Testing Modal Title',
+        emoji: false,
     },
     blocks: [
+        // Plain Text Input
         {
             type: 'input',
+            block_id: 'block_plain-text-input',
             element: {
-                type: 'plain_text_input',
-                action_id: 'sl_input',
-                placeholder: {
-                    type: 'plain_text',
-                    text: 'Placeholder text for single-line input'
-                }
-            },
-            label: {
-                type: 'plain_text',
-                text: 'Label'
-            },
-            hint: {
-                type: 'plain_text',
-                text: 'Hint text'
-            }
-        },
-        {
-            type: 'input',
-            element: {
-                type: 'plain_text_input',
-                action_id: 'ml_input',
+                type: BlockKitInputBlockElementType.PLAIN_TEXT_INPUT,
+                action_id: 'action_plain-text-input',
                 multiline: true,
                 placeholder: {
                     type: 'plain_text',
-                    text: 'Placeholder text for multi-line input'
+                    text: 'Placeholder for plain text input',
+                    emoji: false
+                },
+            },
+            label: {
+                type: 'plain_text',
+                text: 'Plain Text Input',
+                emoji: true
+            }
+        },
+        // Static Select
+        {
+            type: 'input',
+            block_id: 'block_static-select',
+            element: {
+                type: BlockKitInputBlockElementType.STATIC_SELECT,
+                action_id: 'action_static-select',
+                placeholder: {
+                    type: 'plain_text',
+                    text: 'Placeholder for static select',
+                    emoji: true
+                },
+                options: [
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 0',
+                            emoji: true
+                        },
+                        value: 'value-0'
+                    },
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 1',
+                            emoji: true
+                        },
+                        value: 'value-1'
+                    }
+                ]
+            },
+            label: {
+                type: 'plain_text',
+                text: 'Static Select',
+                emoji: true
+            }
+        },
+        // Multiple Static Select
+        {
+            type: 'input',
+            block_id: 'block_multi-static-select',
+            element: {
+                type: BlockKitInputBlockElementType.MULTI_STATIC_SELECT,
+                action_id: 'action_multi-static-select',
+                placeholder: {
+                    type: 'plain_text',
+                    text: 'Placeholder for multiple static select',
+                    emoji: true
+                },
+                options: [
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 0',
+                            emoji: true
+                        },
+                        value: 'value-0'
+                    },
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 1',
+                            emoji: true
+                        },
+                        value: 'value-1'
+                    }
+                ]
+            },
+            label: {
+                type: 'plain_text',
+                text: 'Multiple Static Select',
+                emoji: true
+            }
+        },
+        // Multiple Users Select (Not Supported in Rocket.Chat)
+        {
+            type: 'input',
+            block_id: 'block-multi-users-select',
+            element: {
+                type: BlockKitInputBlockElementType.MULTI_USERS_SELECT,
+                action_id: 'action_multi-users-select',
+                placeholder: {
+                    type: 'plain_text',
+                    text: 'Placeholder for multiple users select',
+                    emoji: true
                 }
             },
             label: {
                 type: 'plain_text',
-                text: 'Label'
+                text: 'Multiple Users Select',
+                emoji: true
+            }
+        },
+        // Date Picker (Not Supported in Rocket.Chat)
+        {
+            type: 'input',
+            block_id: 'block_datepciker',
+            element: {
+                type: BlockKitInputBlockElementType.DATE_PICKER,
+                action_id: 'action_datepicker',
+                initial_date: '1990-04-28',
+                placeholder: {
+                    type: 'plain_text',
+                    text: 'Placeholder for date picker',
+                    emoji: true
+                }
             },
-            hint: {
+            label: {
                 type: 'plain_text',
-                text: 'Hint text'
+                text: 'Date Picker',
+                emoji: true
+            }
+        },
+        // Checkboxes (Not Supported in Rocket.Chat)
+        {
+            type: 'input',
+            block_id: 'block_checkboxes',
+            element: {
+                type: BlockKitInputBlockElementType.CHECKBOXES,
+                action_id: 'action_checkboxes',
+                options: [
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 0',
+                            emoji: true
+                        },
+                        value: 'value-0'
+                    },
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 1',
+                            emoji: true
+                        },
+                        value: 'value-1'
+                    }
+                ]
+            },
+            label: {
+                type: 'plain_text',
+                text: 'Checkboxes',
+                emoji: true
+            }
+        },
+        // Radio Buttons (Not Supported in Rocket.Chat)
+        {
+            type: 'input',
+            block_id: 'block_radio-buttons',
+            element: {
+                type: BlockKitInputBlockElementType.RADIO_BUTTONS,
+                action_id: 'action_radio-buttons',
+                options: [
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 0',
+                            emoji: true
+                        },
+                        value: 'value-0'
+                    },
+                    {
+                        text: {
+                            type: 'plain_text',
+                            text: 'option 1',
+                            emoji: true
+                        },
+                        value: 'value-1'
+                    }
+                ]
+            },
+            label: {
+                type: 'plain_text',
+                text: 'Radio Buttons',
+                emoji: true
             }
         }
     ],
@@ -66,6 +266,7 @@ export const mockBlockKitView: View = {
     callback_id: 'this_is_callback_id',
     clear_on_close: false,
     notify_on_close: false,
+    state: mockBlockKitViewState,
 }
 
 export const mockUIKitView: IUIKitView = {
@@ -75,7 +276,7 @@ export const mockUIKitView: IUIKitView = {
     type: UIKitViewType.MODAL,
     title: {
         type: TextObjectType.PLAINTEXT,
-        text: 'Modal Title',
+        text: 'Testing Modal Title',
         emoji: false
     },
     blocks: [
@@ -137,7 +338,7 @@ export const mockUIKitView: IUIKitView = {
         },
         actionId: '9a340792-bb6a-11ea-b7cf-0319c4330df2'
     },
-    state: {},
+    state: mockUIKitViewState,
     clearOnClose: true,
     notifyOnClose: false
 }
