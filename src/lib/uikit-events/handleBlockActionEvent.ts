67a31722724f68ee06e87ebdbf774ecde5a37063
Douglas Gubert <douglas.gubert@gmail.com>
2020-07-12 19:57:28 -0300

    Complete block_actions notification

--- src/lib/uikit-events/handleBlockActionEvent.ts
+++ src/lib/uikit-events/handleBlockActionEvent.ts
@@ -1,17 +1,31 @@
-import { IUIKitResponse, UIKitBlockInteractionContext } from '@rocket.chat/apps-engine/definition/uikit';
+import { IUIKitResponse, UIKitBlockInteractionContext, IBlock, BlockType, IActionsBlock, IBlockElement, IInteractiveElement, BlockElementType, IButtonElement, IOverflowMenuElement, ISectionBlock } from '@rocket.chat/apps-engine/definition/uikit';
 import { IPersistence, IModify } from '@rocket.chat/apps-engine/definition/accessors';
+import { IUIKitBlockIncomingInteraction } from '@rocket.chat/apps-engine/definition/uikit/UIKitIncomingInteractionTypes';
 import { SlackCompatibleApp } from '../../../SlackCompatibleApp';
-import { BlockKitTextObject, BlockKitEventType, IBlockKitBlockActionsEventPayload, BlockKitBlockActionContainerType } from '../../customTypes/slack';
+import { BlockKitEventType, IBlockKitBlockActionsEventPayload, BlockKitBlockActionContainerType, IBlockKitBlockAction } from '../../customTypes/slack';
 import { getTeamFields, getUserFields, generateResponseUrl, getChannelFields } from '../slackCommonFields';
-import { OriginalActionType, persistResponseToken } from '../ResponseTokens';
+import { OriginalActionType, persistResponseToken } from '../../storage/ResponseTokens';
 import { generateCompatibleTriggerId } from '../../helpers';
 import { UIKitIncomingInteractionContainerType } from '@rocket.chat/apps-engine/definition/uikit/UIKitIncomingInteractionContainer';
 import { convertMessageToSlack } from '../../converters/message';
+import { retrieveView } from '../../storage/PersistView';
+import { convertToBlockKit as convertTextObjectToBlockKit } from "../../converters/objects/text";
+import { convertToBlockKit as convertOptionObjectToBlockKit } from "../../converters/objects/option";
+import { convertBlocksToUIKit } from '../../converters/BlockKitToUIKit';
+
+type BlockActionPayloadExtraFields = Omit<IBlockKitBlockActionsEventPayload, 'type' | 'trigger_id' | 'container' | 'user' | 'team' | 'api_app_id' | 'actions'>;
 
 export async function handleBlockActionEvent(context: UIKitBlockInteractionContext, app: SlackCompatibleApp, persistence: IPersistence, modify: IModify): Promise<IUIKitResponse> {
     const incomingInteraction = context.getInteractionData();
 
-    const extraFields = {};
+    const extraFields: BlockActionPayloadExtraFields = {};
+    const actionDescriptor = {
+        type: '', // type of the element that received the interaction, extracted from the container
+        block_id: incomingInteraction.blockId,
+        value: incomingInteraction.value,
+        action_id: incomingInteraction.actionId,
+        action_ts: String((Date.now() / 1000)),
+    };
     const container = {
         type: BlockKitBlockActionContainerType.MESSAGE,
     };
@@ -37,16 +51,18 @@ export async function handleBlockActionEvent(context: UIKitBlockInteractionConte
             channel: await getChannelFields(incomingInteraction.room),
             response_url: responseUrl,
         });
+
+        Object.assign(actionDescriptor, parseAction(incomingInteraction.message.blocks, incomingInteraction));
     } else if (incomingInteraction.container.type === UIKitIncomingInteractionContainerType.VIEW) {
         Object.assign(container, {
             type: BlockKitBlockActionContainerType.VIEW,
             view_id: incomingInteraction.container.id,
         });
 
-        // @TODO d-gubert fetch the view persisted by `views.open` endpoint
-        Object.assign(extraFields, {
-            view: {},
-        })
+        const view = await retrieveView(incomingInteraction.container.id, app.getAccessors().reader);
+
+        Object.assign(extraFields, { view });
+        Object.assign(actionDescriptor, parseAction(convertBlocksToUIKit(view.blocks), incomingInteraction));
     }
 
     const eventPayload: IBlockKitBlockActionsEventPayload = {
@@ -57,19 +73,67 @@ export async function handleBlockActionEvent(context: UIKitBlockInteractionConte
         api_app_id: incomingInteraction.appId,
         trigger_id: generateCompatibleTriggerId(incomingInteraction.triggerId, incomingInteraction.user),
         ...extraFields,
-        actions: [
-            {
-                type: '', // we don't have this information
-                block_id: '', // we don't have this information
-                text: {} as BlockKitTextObject, // we don't have this information
-                value: incomingInteraction.value,
-                action_id: incomingInteraction.actionId,
-                action_ts: String((Date.now() / 1000))
-            }
-        ]
+        actions: [actionDescriptor],
     };
 
     await app.sendInteraction(eventPayload);
 
     return context.getInteractionResponder().successResponse();
+}
+
+export function parseAction(blocks: Array<IBlock>, incomingInteraction: IUIKitBlockIncomingInteraction): Partial<IBlockKitBlockAction> {
+    const block = blocks.find(block => block.blockId === incomingInteraction.blockId);
+
+    if (!block) {
+        throw new Error('Invalid block provided');
+    }
+
+    const element = (() => {
+        switch (block.type) {
+            case BlockType.ACTIONS: {
+                return (block as IActionsBlock).elements.find(
+                    (element) => isInteractiveElement(element) && element.actionId === incomingInteraction.actionId
+                );
+            }
+
+            case BlockType.SECTION: {
+                return (block as ISectionBlock).accessory;
+            }
+        }
+    })();
+
+    if (!isInteractiveElement(element)) {
+        throw new Error('Invalid action provided');
+    }
+
+    return extractActionDataFromElement(element, incomingInteraction);
+}
+
+function isInteractiveElement(element?: IBlockElement): element is IInteractiveElement {
+    return !element || !!(element as IInteractiveElement).actionId;
+}
+
+export function extractActionDataFromElement(element: IInteractiveElement, incomingInteraction: IUIKitBlockIncomingInteraction): Partial<IBlockKitBlockAction> {
+    switch (element.type) {
+        case BlockElementType.BUTTON:
+            const button = element as IButtonElement;
+            return {
+                type: button.type,
+                text: convertTextObjectToBlockKit(button.text),
+                value: button.value,
+            };
+
+        case BlockElementType.OVERFLOW_MENU:
+            const menu = element as IOverflowMenuElement;
+            const option = menu.options.find(option => option.value === incomingInteraction.value);
+
+            if (!option) {
+                throw new Error('Invalid interaction');
+            }
+
+            return {
+                type: menu.type,
+                selectedOption: convertOptionObjectToBlockKit(option),
+            };
+    }
 }
--- src/storage/PersistView.ts
+++ src/storage/PersistView.ts
@@ -1,8 +1,19 @@
-import { IPersistence } from "@rocket.chat/apps-engine/definition/accessors";
+import { IPersistence, IRead } from "@rocket.chat/apps-engine/definition/accessors";
 import { RocketChatAssociationModel, RocketChatAssociationRecord } from '@rocket.chat/apps-engine/definition/metadata';
-import { IUIKitView } from '@rocket.chat/apps-engine/definition/uikit';
+import { IBlockKitView } from '../customTypes/slack';
 
-export async function PersistView(view: IUIKitView, persis: IPersistence): Promise<void> {
+export async function persistView(view: IBlockKitView, persis: IPersistence): Promise<string> {
     const association = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, view.id);
-    await persis.createWithAssociation({ view }, association);
+    return persis.createWithAssociation({ view }, association);
+}
+
+export async function retrieveView(viewId: IBlockKitView["id"], read: IRead): Promise<IBlockKitView | undefined> {
+    const association = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, viewId);
+    const [result] = await read.getPersistenceReader().readByAssociation(association);
+    return result as IBlockKitView | undefined;
+}
+
+export async function deleteView(viewId: IBlockKitView["id"], persis: IPersistence): Promise<void> {
+    const association = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, viewId);
+    return persis.removeByAssociation(association).then(()=>{});
 }
