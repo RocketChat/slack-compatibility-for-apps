a7ca7b69453e23c25103ed8899100c180f1d2cd4
thassiov <tvmcarvalho@gmail.com>
2020-07-16 18:32:57 -0300

    refactor: centralize duplicated logic from `ViewsOpen` and `ViewsUpdate`

---
+++ src/endpoints/ViewsEndpointHelpers.ts
@@ -0,0 +1,69 @@
+import { IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IUIKitView } from '@rocket.chat/apps-engine/definition/uikit';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
+import { IBlockKitView } from '../customTypes/slack';
+import { convertViewToUIKit } from '../converters/BlockKitToUIKit';
+import { uuid } from '../helpers';
+import { persistView } from '../storage/PersistView';
+import { parseCompatibleTriggerId } from '../helpers';
+
+/**
+ * Handles parsing of an incoming trigger_id
+ *
+ * @param trigger_id string The incoming triggerId in the format "triggerId.userId"
+ * @param read IRead
+ *
+ * @returns Promise<[string, IUser]>
+ */
+export async function incomingTriggeridHandler(trigger_id: string, read: IRead): Promise<[string, IUser]> {
+        if(!trigger_id) {
+            throw new Error('No trigger_id provided');
+        }
+
+        const [triggerId, userId] = parseCompatibleTriggerId(trigger_id);
+
+        if(!triggerId) {
+            throw new Error('No triggerId provided');
+        }
+
+        if (!userId) {
+            throw new Error('No userId provided');
+        }
+
+        const user = await read.getUserReader().getById(userId);
+
+        return [triggerId, user];
+}
+
+/**
+ * Handles parsing and storage of an incoming view
+ *
+ * @param view any The object that represents the view
+ * @param persis IPersistence
+ *
+ * @returns Promise<IUIKitView>
+ */
+export async function incomingViewHandler(view: any, persis: IPersistence): Promise<IUIKitView> {
+    const slackView = (() => {
+        if (typeof view !== 'string') return view;
+
+        try {
+            return JSON.parse(view);
+        } catch {
+            return undefined;
+        }
+    })() as IBlockKitView | undefined;
+
+    if (!slackView) {
+        throw new Error('Invalid view definition');
+    }
+
+    // We need this to store the view
+    if (!slackView.id) {
+        slackView.id = uuid();
+    }
+
+    await persistView(slackView, persis);
+
+    return convertViewToUIKit(slackView, this.app.getID());
+}
--- src/endpoints/ViewsOpen.ts
+++ src/endpoints/ViewsOpen.ts
@@ -1,10 +1,6 @@
 import { IHttp, IModify, IPersistence, IRead, HttpStatusCode } from '@rocket.chat/apps-engine/definition/accessors';
 import { ApiEndpoint, IApiEndpointInfo, IApiRequest, IApiResponse } from '@rocket.chat/apps-engine/definition/api';
-import { convertViewToUIKit } from '../converters/BlockKitToUIKit';
-import { parseCompatibleTriggerId, uuid } from '../helpers';
-import { persistView } from '../storage/PersistView';
-import { IBlockKitView } from '../customTypes/slack';
-
+import { incomingTriggeridHandler, incomingViewHandler } from './ViewsEndpointHelpers';
 
 export class ViewsOpen extends ApiEndpoint {
     public path = 'views.open';
@@ -13,40 +9,18 @@ export class ViewsOpen extends ApiEndpoint {
     public async post(request: IApiRequest, endpoint: IApiEndpointInfo, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<IApiResponse> {
         const { view, trigger_id } = request.content;
 
-        if(!view || !trigger_id) {
-            return this.json({ status: HttpStatusCode.BAD_REQUEST });
+        let uikitView, triggerId, user;
+        try {
+            [uikitView, [triggerId, user]] = await Promise.all([
+                incomingViewHandler(view, persis),
+                incomingTriggeridHandler(trigger_id, read),
+            ]);
+        } catch (viewHandlingError) {
+            return this.json({
+                status: HttpStatusCode.BAD_REQUEST,
+                content: { success: false, message: viewHandlingError.message || '' }
+            });
         }
-
-        const [triggerId, userId] = parseCompatibleTriggerId(trigger_id);
-
-        if(!triggerId || !userId) {
-            return this.json({ status: HttpStatusCode.BAD_REQUEST });
-        }
-
-        const slackView = (() => {
-            if (typeof view !== 'string') return view;
-
-            try {
-                return JSON.parse(view);
-            } catch {
-                return undefined;
-            }
-        })() as IBlockKitView | undefined;
-
-        if (!slackView) {
-            return this.json({ status: HttpStatusCode.BAD_REQUEST, content: { success: false, message: 'Invalid view definition' } });
-        }
-
-        // We need this to store the view
-        if (!slackView.id) {
-            slackView.id = uuid();
-        }
-
-        await persistView(slackView, persis);
-
-        const uikitView = convertViewToUIKit(slackView, this.app.getID());
-
-        const user = await read.getUserReader().getById(userId);
 
         await modify.getUiController().openModalView(uikitView, { triggerId }, user);
 
